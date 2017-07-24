176cebfc4aa971b33f97e92deb0945a1b95f2c6c
gwiedeman <gregory.wiedeman1@gmail.com>
2017-07-24 14:39:23 -0400

    simplified the workflow and wrote docs

--- README.md
+++ README.md
@@ -1,2 +1,128 @@
 # asInventory
 Manage file-level ArchivesSpace inventories with spreadsheets
+
+### Warning note
+
+This tool has been tested with ArchivesSpace 1.5.x on Windows at UAlbany. The Python source should also run on Mac and Linux with dependencies, but is generally untested.
+
+This tool makes iterative changes through the API. You should always do significant testing on a development instance of ASpace before using it on production data to make sure it acts as you expect.
+
+## Basic Use
+
+1. First download and extract [the zip file](https://github.com/UAlbanyArchives/archives_tools/archive/master.zip) or clone the repo with git:
+
+	```
+	git clone https://github.com/UAlbanyArchives/asInventory
+	```
+
+2. Edit the sample config file `local_settings_sample.cfg` with your ArchivesSpace credentials and rename to `local_settings.cfg` 
+	* (`[asInventory]` section is only required for the dao upload workflow, you can add digital objects without editing these if you just enter URLs.)
+3. asInventory requires three directories that will be created when you first run it:
+	```
+	input
+	output
+	complete
+	dao
+	```
+
+#### To export an inventory
+
+1. run asDownload.exe (may take a minute to open the first time)
+	![](screenshots/screenshot1.png)
+2. Select the level to export
+	* Select "Resource" to export a folder list from a collection that has no series
+	* Select "Archival Object" to export a folder list from a series, sub series, or other component
+3. Paste in the ID for the parent of the folder list you want to export
+	* For Resource, use id_0
+	![](screenshots/screenshot2.png)
+	* For other components use Ref ID
+	![](screenshots/screenshot3.png)
+4. Click "OK" and a list of files exported will print to the console. This may take some time for large file listings.
+5. If the export is successful, you will be given the option to open the output directory to view the exported file
+6. A new Spreadsheet file will be placed in the `output` directory. **WARNING: files with the same name in this directory will be overwritten.**
+	![](screenshots/screenshot4.png)
+
+#### To import an inventory
+
+1. Make a copy of asInventory.xlsx, you can name it anything you'd like.
+2. Open the spreadsheet and add a folder listing
+	* Mandatory fields:
+		* Level (I2) must be "resource" for collection level with no series, or "archival object"
+		* RefID (I4) must be id_0 for resource parent or ref_id for archival object parent
+	* Title (I1) is not mandatory and can be anything, sheet name can also be anything
+	* Many columns can be left blank
+	* If an ID is entered, asInventory will find and update an existing record
+	* If no ID is entered, asInventory will create a new archival object child
+	* If URIs for Locations or containers are entered, asInventory will link these records to the new archival object record, if they are left place, new containers and locations will be created if a label and indicator are listed
+	* Accepts up to 5 Dates and uses Normal dates (i.e. "1977/1988" or "1903-03-17/1917-01/15")
+	* Display dates are optional and are entered in the ASpace Expression field
+	* Can make Access Restriction (column T), General Note (column U), and Scope (column V) notes.
+	* Can create and link digital object. This can be a link entered in column W, or the filename of a file placed in the `dao` directory. If dao paths are set up correctly in `local_settings.cfg` asInventory will also rename files to their newly created ASpace IDs and move them to a webserver.
+	* ![](screenshots/screenshot5.png)
+3. Save the spreadsheet to the `input` directory
+4. run asUpload.exe (may take a minute to open the first time)
+	* ![](screenshots/screenshot5.png)
+5. The spreadsheet file will be moved into the `complete` directory after the upload is completed. **WARNING: files with the same name in this directory will be overwritten.**
+
+## Prerequisites
+
+The executable files should have no dependencies on Windows other than ArchivesSpace. asInventory has been tested on 1.5.x 
+
+Running the Python source requires Python some non-standard libraries, and the custom archives_tools library:
+
+* [configparser](https://docs.python.org/3/library/configparser.html)
+* [openpyxl](https://openpyxl.readthedocs.io/en/default/)
+* [wxPython](https://wxpython.org/)
+* [archives_tools](https://github.com/UAlbanyArchives/archives_tools)
+
+
+#### Installing dependencies to run from Python source
+
+These commands will install the necessary dependencies if you want to run the raw Python instead of the executable files. Python is of course required, and although all the code should be compatible with both Python 2.7 and Python 3, Python 2.7 is probably safer for wxPython.
+
+```
+pip install configparser
+pip install openpyxl
+git clone https://github.com/UAlbanyArchives/archives_tools
+cd archives_tools
+python setup.py install
+```
+For wxPython:
+	
+* For Windows use the [installer](http://www.wxpython.org/download.php), make sure to select the version compatible with your Python
+* For Macs `brew install wxpython` should work
+* For Ubuntu, this should work: `sudo apt-get install python-wxgtk2.8 python-wxtools wx2.8-doc wx2.8-examples wx2.8-headers wx2.8-i18n`
+
+## Additional features
+
+#### Validation
+
+`validate.exe` (and `validate.py`) will validate all dates entered in all spreadsheets in the `input` folder. This should make them compatible with ASpace to reduce errors during uploading. Hopeful more detailed validation to come.
+
+#### Creating digital objects
+
+`daoList.exe` will create `daoList.txt` which will list all files placed in the `dao` directory.
+
+The envisioned use case for this is:
+	1. You have a lot of PDFs (or anything else really) of the folders in the file listing you want to upload.
+	2. You place these PDFs in the `dao` directory
+	3. Run `daoList.exe` to get a text listing of the files
+	4. Paste that listing into the upload spreadsheet in the DAO Filename column (W).
+	5. During upload, asInventory will create ASpace digital objects, rename these files to their ASpace UUIDs, and move them to a webserver path listed in `local_settings.cfg`
+
+## General note on sustainability
+
+This tool in in active use at UAlbany, and thus I'm committed to maintaining this project for at least the near future. It does rely on the custom [archives_tools](https://github.com/UAlbanyArchives/archives_tools) library with makes it relatively small and simple. (asDownload.py is under 400 lines of code, GUI and all). If a community-led ArchivesSpace library for Python is developed, I'll make a strong effort to move asInventory to that library.
+
+
+## Contributing
+
+Comments and pull requests welcome.
+
+## Authors
+
+Greg Wiedeman
+
+## License
+
+This project is in the public domain
--- asDownload.py
+++ asDownload.py
@@ -1,10 +1,15 @@
 import os
 import sys
-import ConfigParser
-from subprocess import Popen, PIPE, STDOUT
 import wx
 import datetime
-import shutil
+import traceback
+from subprocess import Popen, PIPE, STDOUT
+
+#non-standard dependencies
+import configparser
+import openpyxl
+from archives_tools import aspace as AS
+from archives_tools import uaLocations
 
 # GUI Dialog to get Level and ID to Export
 class getCmpntDialog(wx.Dialog):
@@ -96,77 +101,264 @@ class getCmpntDialog(wx.Dialog):
 
 		# get local_settings
 		configPath = os.path.join(__location__, "local_settings.cfg")
-		config = ConfigParser.ConfigParser()
+		config = configparser.ConfigParser()
 		config.read(configPath)
 
 		baseURL = config.get('ArchivesSpace', 'baseURL')
 		repository = config.get('ArchivesSpace', 'repository')
 		user = config.get('ArchivesSpace', 'user')
 		password = config.get('ArchivesSpace', 'password')
+		loginData = (baseURL, user, password)
 		
-		#check if asInventory is updated
-		masterPath = config.get('asInventory', 'path')
-		masterFile = ""
-		for file in os.listdir(masterPath):
-			if file.startswith("asInventory") and file.endswith(".exe"):
-				masterFile = file
-		if len(masterFile) == 0:
-			errorNotice = wx.MessageDialog(None, "Error: Could not find master asInventory file in " + masterPath, 'No Master File', wx.OK | wx.ICON_ERROR )
-			errorNotice.ShowModal()
-		masterVersion = os.path.splitext(masterFile)[0].split("-")[1]
-		localFile = ""
-		for file in os.listdir(__location__):
-			if file.startswith("asInventory") and file.endswith(".exe"):
-				localFile = file
-		localVersion = os.path.splitext(localFile)[0].split("-")[1]
-		if masterVersion > localVersion:
-			print "Updating asInventory"
-			os.remove(os.path.join(__location__, localFile))
-			shutil.copy2(os.path.join(masterPath, masterFile), __location__)
-			asInventory = os.path.join(__location__, masterFile)
-		else:
-			asInventory = os.path.join(__location__, localFile)
 
-		# output path
+		# required directories, make them if they don't exist
+		inputPath = os.path.join(__location__, "input")
+		if not os.path.isdir(inputPath):
+			os.mkdir(inputPath)
 		outputPath = os.path.join(__location__, "output")
 		if not os.path.isdir(outputPath):
 			os.mkdir(outputPath)
-
-
-		cmpntID = self.m_textCtrl2.GetValue()
-		if self.m_radioBtn2.GetValue() == True:
-			level = "resource"
-		else:
-			level = "archivalObject"
-		if len(cmpntID) < 1:
-			noIDNotice = wx.MessageDialog(None, 'Please Enter a Ref ID for an Archival Objector an id_0 for a Resource.', 'Missing ID', wx.OK | wx.ICON_EXCLAMATION )
-			noIDNotice.ShowModal()
-		elif level == "archivalObject" and len(cmpntID) != 32:
-			wrongIDNotice = wx.MessageDialog(None, 'It looks like you selcted archival object, but this is not an archival object ref_id. Check that you have the correct ID or select resource instead.', 'Incorrect ID', wx.OK | wx.ICON_EXCLAMATION)
-			wrongIDNotice.ShowModal()
-		else:
-
-			#build command list
-			cmd = [asInventory, "-download", outputPath, level, cmpntID, baseURL, repository, user, password]
-			# call master asInventory
-			#print (" ".join(cmd))
-			asDownload = Popen(cmd, shell=True, stdout=PIPE, stderr=STDOUT)
+		completePath = os.path.join(__location__, "complete")
+		if not os.path.isdir(completePath):
+			os.mkdir(completePath)
+		daoPath = os.path.join(__location__, "dao")
+		if not os.path.isdir(daoPath):
+			os.mkdir(daoPath)
 			
-			# busy dialog
-			self.Hide()
-			msg = "Please wait while we export the data you requested from ArchivesSpace..."
-			print (msg)
-			#busyDlg = wx.BusyInfo(msg)
+		# Main error handleing
+		try:
+
+			#get ID value entered in GUI
+			cmpntID = self.m_textCtrl2.GetValue()
+			if self.m_radioBtn2.GetValue() == True:
+				level = "resource"
+			else:
+				level = "archivalObject"
+			if len(cmpntID) < 1:
+				noIDNotice = wx.MessageDialog(None, 'Please Enter a Ref ID for an Archival Objector an id_0 for a Resource.', 'Missing ID', wx.OK | wx.ICON_EXCLAMATION )
+				noIDNotice.ShowModal()
+			elif level == "archivalObject" and len(cmpntID) != 32:
+				wrongIDNotice = wx.MessageDialog(None, 'It looks like you selcted archival object, but this is not an archival object ref_id. Check that you have the correct ID or select resource instead.', 'Incorrect ID', wx.OK | wx.ICON_EXCLAMATION)
+				wrongIDNotice.ShowModal()
+			else:
+
+				#build command list
+				cmd = ["cmd", "-download", outputPath, level, cmpntID, baseURL, repository, user, password]
+				
+				# busy dialog
+				self.Hide()
+				msg = "Please wait while we export the data you requested from ArchivesSpace..."
+				print (msg)
+				#busyDlg = wx.BusyInfo(msg)
+				#busyDlg = None
+				
+				print ("do stuff")
+				#Connect to ASpace
+				session = AS.getSession(loginData)
+				if session is None:
+					raise ValueError("ERROR: ArchivesSpace login failed. Please check settings in local_settings.cfg")
+					
+				#create Workbook object
+				wb = openpyxl.Workbook()
+				
+				if level.lower().strip() == "resource":
+					resourceLevel = True
+					print ("Looking for resource")
+				else:
+					resourceLevel = False
+					print ("Looking for  archival object")
+				
+				if resourceLevel == True:
+					object = AS.getResourceID(session, repository, cmpntID, loginData)		
+					displayTitle = object.title.replace("/", "-")
+				else:	
+					object = AS.getArchObjID(session, repository, cmpntID, loginData)
+					displayTitle = object.display_string
+					
+				simpleTitle = object.title.replace("/", "-")
+				print ("Reading " + simpleTitle)
+				
+				
+				if resourceLevel == True:
+					objectID = object.id_0
+				else:
+					objectID = object.ref_id
+				
+				#make a sheet
+				worksheet = wb.active
+				#worksheet = wb.create_sheet(title=simpleTitle)
+				worksheet["H1"] = "Title"
+				worksheet["I1"] = displayTitle
+				worksheet["H2"] = "Level"
+				if resourceLevel == True:
+					worksheet["I2"] = "resource"
+				else:
+					worksheet["I2"] = "archival object"
+				worksheet["H3"] = "Ref ID"
+				worksheet["I3"] = objectID
+				
+				#setup table headings
+				worksheet["A6"] = "ID"
+				worksheet["B6"] = "Location ID"
+				worksheet["C6"] = "Location"
+				worksheet["D6"] = "Container URI"
+				worksheet["E6"] = "Container"
+				worksheet["F6"] = "C#"
+				worksheet["G6"] = "Folder"
+				worksheet["H6"] = "F#"
+				worksheet["I6"] = "Title"
+				worksheet["J6"] = "Date 1 Display"
+				worksheet["K6"] = "Date 1 Normal"
+				worksheet["L6"] = "Date 2 Display"
+				worksheet["M6"] = "Date 2 Normal"
+				worksheet["N6"] = "Date 3 Display"
+				worksheet["O6"] = "Date 3 Normal"
+				worksheet["P6"] = "Date 4 Display"
+				worksheet["Q6"] = "Date 4 Normal"
+				worksheet["R6"] = "Date 5 Display"
+				worksheet["S6"] = "Date 5 Normal"
+				worksheet["T6"] = "Restrictions"
+				worksheet["U6"] = "General Note"
+				worksheet["V6"] = "Scope"
+				worksheet["W6"] = "DAO Filename"
+				
+				#get table styles
+				tableStyle = openpyxl.worksheet.table.TableStyleInfo(name='TableStyleMedium2', showRowStripes=True)
+				
+				if resourceLevel == True:
+					resourceTree = AS.getTree(session, object, loginData)
+					childrenList = resourceTree.children
+				else:
+					childTree = AS.getChildren(session, object, loginData)
+					childrenList = childTree.children
+					
+				lineCount = 6
+				for child in childrenList:
+					childObject = AS.getArchObj(session, child.record_uri, loginData)
+					lineCount = lineCount + 1
+					worksheet["A" + str(lineCount)] = childObject.ref_id
+					worksheet["I" + str(lineCount)] = childObject.title
+					try:
+						print ("	exporting " + childObject.title)
+					except:
+						print ("	exporting non-ascii file...")
+					
+					# containers and locations		 
+					if len(childObject.instances) > 0:
+						if "sub_container" in childObject.instances[0].keys():
+							container = childObject.instances[0].sub_container
+							containerURI = container.top_container.ref
+							worksheet["D" + str(lineCount)] = containerURI
+							if "type_2" in container.keys():
+								worksheet["G" + str(lineCount)] = container.type_2
+							if "indicator_2" in container.keys():
+								worksheet["H" + str(lineCount)] = container.indicator_2
+								
+							containerObject = AS.getContainer(session, containerURI, loginData)
+							worksheet["E" + str(lineCount)] = containerObject.type
+							worksheet["F" + str(lineCount)] = containerObject.indicator
+							
+							locationCount = 0
+							for location in containerObject.container_locations:
+								locationCount = locationCount + 1
+								locationObject = AS.getLocation(session, location.ref, loginData)
+								if "area" in locationObject.keys():
+									locationCoordinates = locationObject.area + "-" + locationObject.coordinate_1_indicator
+								else:
+									locationCoordinates = locationObject.room + "-" + locationObject.coordinate_1_indicator
+								if "coordinate_2_indicator" in locationObject.keys():
+									locationCoordinates = locationCoordinates + "-" + locationObject.coordinate_2_indicator
+								if "coordinate_3_indicator" in locationObject.keys():
+									locationCoordinates = locationCoordinates + "-" + locationObject.coordinate_3_indicator
+								if locationCount < 2:
+									worksheet["B" + str(lineCount)] = locationObject.uri
+									worksheet["C" + str(lineCount)] = locationCoordinates
+								else:
+									worksheet["B" + str(lineCount)] = worksheet["B" + str(lineCount)].value + "; " + locationObject.uri
+									worksheet["C" + str(lineCount)] = worksheet["C" + str(lineCount)].value + "; " + locationCoordinates					
+							
+					
+					#dates
+					dateCount = 0
+					for date in childObject.dates:
+						dateCount = dateCount + 1
+						if dateCount == 1:
+							displayCell = "J"
+							normalCell = "K"
+						elif dateCount == 2:
+							displayCell = "L"
+							normalCell = "M"
+						elif dateCount == 3:
+							displayCell = "N"
+							normalCell = "O"
+						elif dateCount == 4:
+							displayCell = "P"
+							normalCell = "Q"
+						elif dateCount == 5:
+							displayCell = "R"
+							normalCell = "S"
+						elif dateCount > 5:
+							raise ValueError("ERROR more than 5 dates for " + "uri: " + childObject.uri + " ref_id: " + childObject.ref_id)
+						if "end" in date.keys():
+							worksheet[normalCell + str(lineCount)] = date.begin + "/" + date.end
+						else:
+							worksheet[normalCell + str(lineCount)] = date.begin
+						if "expression" in date.keys():
+							worksheet[displayCell + str(lineCount)] = date.expression
+						if "certainty" in date.keys():
+							if worksheet[displayCell + str(lineCount)].value is None:
+								worksheet[displayCell + str(lineCount)] = date.certainty
+							else:
+								worksheet[displayCell + str(lineCount)] = date.certainty + " " + worksheet[displayCell + str(lineCount)].value
+					
+					for note in childObject.notes:
+						if note.type == "accessrestrict":
+							subCount = 0
+							for subnote in note.subnotes:
+								subCount = subCount + 1
+								if subCount < 1:
+									worksheet["T" + str(lineCount)] = worksheet["T" + str(lineCount)] + "; " +  subnote.content
+								else:
+									worksheet["T" + str(lineCount)] = subnote.content
+						elif note.type == "odd":
+							subCount = 0
+							for subnote in note.subnotes:
+								subCount = subCount + 1
+								if subCount < 1:
+									worksheet["U" + str(lineCount)] = worksheet["U" + str(lineCount)] + "; " +  subnote.content
+								else:
+									worksheet["U" + str(lineCount)] = subnote.content
+						elif note.type == "scopecontent":
+							subCount = 0
+							for subnote in note.subnotes:
+								subCount = subCount + 1
+								if subCount < 1:
+									worksheet["V" + str(lineCount)] = worksheet["V" + str(lineCount)] + "; " +  subnote.content
+								else:
+									worksheet["V" + str(lineCount)] = subnote.content
+					
+				print ("Writing spreadsheet " + simpleTitle + ".xlsx to " + outputPath)
+				
+				table = openpyxl.worksheet.table.Table(ref='A6:W' + str(lineCount), displayName='Inventory', tableStyleInfo=tableStyle)
+				worksheet.add_table(table)
+				
+				#styles for sheet info on top
+				worksheet["H1"].style = "Accent1"
+				worksheet["H2"].style = "Accent1"
+				worksheet["H3"].style = "Accent1"
+				
+				#set column widths
+				worksheet.column_dimensions["I"].width = 60.0
+				worksheet.column_dimensions["F"].width = 15.0
+				worksheet.column_dimensions["J"].width = 15.0
+				worksheet.column_dimensions["K"].width = 15.0
+				
+				
+				wb.save(filename = os.path.join(outputPath, simpleTitle + ".xlsx"))
+				print ("Export Complete!")
 			
-			output = ""
-			for line in iter(asDownload.stdout.readline, ""):
-				print (line)
-				output += line
-			asDownload.wait()
-			exitCode = asDownload.returncode
-			#busyDlg = None
-			if exitCode == 0:
-				successNotice = wx.MessageDialog(None, "Export Successful.\n\nSuccessfully exported archival object from ArchivesSpace to spreadsheet at " + masterPath + ".\n\nWould you like to open the ouput folder?" , 'Export Successful', wx.YES_NO | wx.ICON_INFORMATION)
+
+				successNotice = wx.MessageDialog(None, "Export Successful.\n\nSuccessfully exported archival object from ArchivesSpace to spreadsheet at " + outputPath + ".\n\nWould you like to open the ouput folder?" , 'Export Successful', wx.YES_NO | wx.ICON_INFORMATION)
 				successResponse = successNotice.ShowModal()
 				if successResponse == wx.ID_YES:
 					openCmd = "start " + outputPath
@@ -174,15 +366,17 @@ class getCmpntDialog(wx.Dialog):
 					stdout, stderr = openDir.communicate()
 					print stdout
 					print stderr
-			else:
-				outputText = "asDownload error: " + output
-				errorOutput = "\n" + "#############################################################\n" + str(datetime.datetime.now()) + "\n#############################################################\n" + outputText + "\n********************************************************************************"
-				file = open(os.path.join(__location__, "error.log"), "a")
-				file.write(errorOutput)
-				file.close()
-				errorNotice = wx.MessageDialog(None, "Error exporting archival object from ArchivesSpace. Please check error.log for more details. " + output, 'Export Error', wx.OK | wx.ICON_ERROR )
-				errorNotice.ShowModal()
-				
+				self.Destroy()
+		except:
+			exceptMsg = traceback.format_exc()
+			outputText = "asDownload error: " + exceptMsg
+			errorOutput = "\n" + "#############################################################\n" + str(datetime.datetime.now()) + "\n#############################################################\n" + outputText + "\n********************************************************************************"
+			file = open(os.path.join(__location__, "error.log"), "a")
+			file.write(errorOutput)
+			file.close()
+			errorNotice = wx.MessageDialog(None, "Error exporting archival object from ArchivesSpace. Please check error.log for more details. " + exceptMsg, 'Export Error', wx.OK | wx.ICON_ERROR )
+			errorNotice.ShowModal()
+			
 			self.Destroy()
 
 # Initialize wx App
